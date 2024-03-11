6d36cc818581bc43cda95363e1899934573c8aaa
TevinG777 <gajadhartevin@gmail.com>
2024-03-11 11:46:24 -0400

    OUTPUT GOOD

--- CompileDCM.ps1
+++ CompileDCM.ps1
@@ -1,5 +1,7 @@
 $linkStructure = "\\ad101.siemens-energy.net\dfs\EnergyFS\NAM\GST_Application Data\NDT\DR\WAX DCM"
 
+#define location list
+$locationList = @()
 
 Get-Content -Path 'partsDCM.txt' | ForEach-Object {
     # Get the first 5 characters as the part variable
@@ -17,54 +19,64 @@ Get-Content -Path 'partsDCM.txt' | ForEach-Object {
     # Look for Directories in the file path with file name matching the search pattern
     $files = Get-ChildItem -Path $linkStructurePart -Filter $searchPattern -Directory
 
-    #     # If more than one folder is found, select the first occurrence
-    #     if ($files.Count -gt 1) {
-    #         $files = $files[0]
-    #
-    #         #Take the folder location as a string and split it by \ to get the last folder name
-    #         $folderName = $files.FullName.Split("\")[-1]
-    #
-    #
-    #
-    #         # Split that string by space and determine which part of the string matches the number, if it is in the first index assign location variable to 1 and so on up to 3
-    #         $location = 0
-    #         $partList = $folderName.Split(" ")
-    #         $partList | ForEach-Object {
-    #             if($_ -eq $num){
-    #                 break
-    #             }
-    #             $location++
-    #         }
-    #
-    #     }
-    #
-    #     # If no file is found
-    #     elseif ($files.Count -eq 0) {
-    #         "$_ = \\ad101.siemens-energy.net\dfs\EnergyFS\NAM\GST_Application Data\NDT\DR\WAX PNG\NotFound.jpg" | Out-File -FilePath 'outputDCM.txt' -Append
-    #     }
-    if ($files.Count -eq 1) {
-        $num
-        #Take the folder location as a string and split it by \ to get the last folder name
-        $folderName = $files.FullName.Split("\")[-1]
-
-        # remove any commas from the folder name
-        $folderName = $files.FullName.Replace(",", "")
-
-        # Split that string by space and determine which part of the string matches the number, if it is in the first index assign location variable to 1 and so on up to 3
-        $location = 1
-        $folderName.Split(" ") | ForEach-Object {
-            if($_ -eq $num){
-                break
-            }
-            $location++
-        }
-         ## Then go into the folder defined by $file.FullName and get all the files that start with "Core " + $location
-         #$coreFiles = Get-ChildItem -Path $files.FullName -Filter ("Core " + $location + "*") -File
-    
-         ## For each one of these files, get the full path and append it to the output file with the beginning part being $part+$num
-         #$coreFiles | ForEach-Object {
-         #    "$part$num = " + $_.FullName | Out-File -FilePath 'outputDCM.txt' -Append
-         #}
+    if ($files.Count -eq 0) {
+        #"$_ = \\ad101.siemens-energy.net\dfs\EnergyFS\NAM\GST_Application Data\NDT\DR\WAX PNG\NotFound.jpg" | Out-File -FilePath 'outputDCM.txt' -Append
     }
-    
+    else {     
+        # Take the folder location as a string and split it by \ to get the last folder name
+        $folderSection = $files.FullName.Split("\")[-1].Replace(",","").Split(" ")
+        # location
+        $location = 1
+        # print each folder name as a set to console
+        $folderSection | ForEach-Object {
+            # if $_ is equal to $num then leave location at current value, else update location by 1
+            if($_ -eq $num){
+                # append value of location to location list
+                $locationList += $location
+            }
+            else{
+                $location++
+            }
+        }
+    }
+}
+
+$count = 0
+Get-Content -Path 'partsDCM.txt' | ForEach-Object {
+    # Get the first 5 characters as the part variable
+    $part = $_.Substring(0, 5)
+
+    # Get the remaining characters as the num variable
+    $num = $_.Substring(5)
+
+    # Append the part variable to the linkStructure variable as a subdirectory
+    $linkStructurePart = Join-Path -Path $linkStructure -ChildPath ($part + "\")
+
+    # Define the search pattern for files
+    $searchPattern = "*$num*"
+
+    # Look for Directories in the file path with file name matching the search pattern
+    $files = Get-ChildItem -Path $linkStructurePart -Filter $searchPattern -Directory
+
+    # output all files found
+    if ($files.Count -eq 0) {
+        # output not found to outputDCM.txt
+        "$_ = \\ad101.siemens-energy.net\dfs\EnergyFS\NAM\GST_Application Data\NDT\DR\WAX PNG\NotFound.jpg" | Out-File -FilePath 'outputDCMt.txt' -Append
+    }
+    else {
+
+        $directoryPath = $files.FullName
+        # get the location from the location list
+        $location = $locationList[$count]
+        # get the file with the name "Core" + location
+        $coreFile = Get-ChildItem -Path $directoryPath -Filter "Core $location*"
+        # output all files found to outputDCM.txt
+        $coreFile | ForEach-Object {
+            "$part$num = " +$_.FullName | Out-File -FilePath 'outputDCMt.txt' -Append
+        }
+        $count++
+        
+
+    }
+        
 }
