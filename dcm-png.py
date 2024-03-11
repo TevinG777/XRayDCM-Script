f9945870edd206d8cedad05b30aaff80724b05e0
TevinG777 <gajadhartevin@gmail.com>
2024-03-11 13:27:00 -0400

    updated script

--- dcm-png.py
+++ dcm-png.py
@@ -3,23 +3,52 @@ import os
 import pydicom
 from pydicom.pixel_data_handlers.util import apply_voi_lut
 import numpy as np
+import re
 
-inputdir='./dcmFiles/'
-outdir='./pngFiles/'
-if not os.path.exists(outdir):
-    os.makedirs(outdir)
+# Process the output.txt file      
+with open(r"outputDCMt.txt", "r") as file:      
+    lines = file.readlines()      
+  
+processed_parts = []      
+processed_paths = []       
+for line in lines:      
+    line = line.strip('\x00ÿþ\n').replace('\\', '\\\\')
+            
+    if line:      
+        parts = line.split("=")      
+        if len(parts) == 2:      
+            path = parts[1]
+            processed_parts.append(parts[0])      
+            processed_paths.append(path[5:])      
 
-# loop through all the DICOM files
-for f in os.listdir(inputdir):
-    ds = pydicom.dcmread(os.path.join(inputdir, f))  # read the dcm file
-    img = ds.pixel_array  # get the image array
+            
+        else:      
+            print("Invalid line format:", line)   
+#Create a folder to save the images
+if not os.path.exists('dcmImgsFolder'):
+    os.makedirs('dcmImgsFolder')
 
-    #Normalize the image and negate it
-    img = apply_voi_lut(img, ds)  # apply the VOI LUT (if available)
-    img = img - np.min(img)  # scale the image
-    img = img / np.max(img)  # scale the image
-    img = (img * 255).astype(np.uint8)  # convert the image to 8-bit unsigned int
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)  # convert the image to 3 channels
-    img = cv2.bitwise_not(img)  # invert the image
+# Convert the dcm files to png
+for i in range(len(processed_paths)):
+    path = str(processed_paths[i]).strip('\x00') 
+    path = re.sub(r'(?<=.)\x00(?=.)', '', path)
+
+    # if path ends with .dcm, then convert to .png
+    if path.endswith('.dcm'):
+        # Read the dcm file
+        ds = pydicom.dcmread(path)
+        # Apply the VOI LUT transformation
+        img = ds.pixel_array  # get the image array
+        img = apply_voi_lut(img, ds)  # apply the VOI LUT (if available)
+        img = img - np.min(img)  # scale the image
+        img = img / np.max(img)  # scale the image
+        img = (img * 255).astype(np.uint8)  # convert the image to 8-bit unsigned int
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)  # convert the image to 3 channels
+        img = cv2.bitwise_not(img)  # invert the image
+        
+        # Save the image as .png in dcmImgsFolder in current directory with same original name
+        cv2.imwrite('dcmImgsFolder/' + os.path.basename(path) + '.png', img)
+
+        print("saved in dcmImgsFolder: ", os.path.basename(path) + ".png")
+
     
-    cv2.imwrite(os.path.join(outdir, f.replace('.dcm', '.png')), img)  # write png image
---
+++ removeFolder.ps1
@@ -0,0 +1,2 @@
+#remove the dcmImg folder even if it is full
+Remove-Item -LiteralPath "dcmImgsFolder" -Force -Recurse
